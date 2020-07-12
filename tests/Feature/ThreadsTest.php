a25e67917953cfc34ced8e8325097dbec507ef7c
Vagelis Giannakosian <e.giannakosian@gmail.com>
2020-07-12 12:17:51 +0300

    Added auth scaffolding Added phpunit tests config Added vue config

---
+++ tests/Feature/ThreadsTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ThreadsTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
+
+        $response->assertStatus(200);
+    }
+}
