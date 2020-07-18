b67ef40adaa2c2637cf94052fd3415adefbe7278
Vagelis Giannakosian <e.giannakosian@gmail.com>
2020-07-18 11:52:27 +0300

    Extracting to View Composers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+//        View::composer(['threads.create','threads.index','*'],function($view){
+//            $view->with('channels',Channel::all());
+//        });
+
         if(app()->environment() !== 'testing')
         {
             View::share('channels', Channel::all());
