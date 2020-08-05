ed1a8ff2fc807abd504228cb082c40f5fa768a44
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-08-05 11:51:43 +0300

    Testing Avatar Uploads

---
+++ app/Http/Controllers/Api/UserAvatarController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\User;
+use Illuminate\Http\Request;
+
+class UserAvatarController extends Controller
+{
+
+    public function store(User $user)
+    {
+        request()->validate([
+            'avatar' => 'required|image'
+        ]);
+
+        auth()->user()->update([
+            'avatar_path' =>request()->file('avatar')->store('avatars','public')
+        ]);
+
+        return back();
+    }
+}
---
+++ tests/Feature/AddAvatarTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tests\Feature;
+
+
+use App\Reply;
+use App\User;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Tests\TestCase;
+use \Illuminate\Foundation\Testing\RefreshDatabase;
+
+class AddAvatarTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function test_only_members_can_add_avatars()
+    {
+        $this->withoutExceptionHandling();
+        $user = create(User::class);
+
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $this->post(route('api.userAvatar.store',$user));
+    }
+
+
+    function test_a_valid_avatar_must_be_provided()
+    {
+        $user = $this->withExceptionHandling()->signIn();
+
+        $this->post(route('api.userAvatar.store',$user),[
+            'avatar' => 'not-an-image'
+        ])->assertStatus(302);
+    }
+
+    function test_a_user_may_add_an_avatar_to_their_profile()
+    {
+        $this->signIn();
+
+        Storage::fake('public');
+
+        $this->post(route('api.userAvatar.store',auth()->user()),[
+            'avatar' => $file = UploadedFile::fake()->image('avatar.jpg')
+        ]);
+
+
+        $this->assertEquals('avatars/'.$file->hashName(),auth()->user()->avatar_path);
+
+        Storage::disk('public')->assertExists('avatars/'.$file->hashName());
+    }
+
+}
